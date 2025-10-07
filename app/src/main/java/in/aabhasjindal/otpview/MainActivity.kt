d8730601f81453818122b23139b838f95228b10c
Leonardo D. Palma <leonardo.palma13@est.unanleon.edu.ni>
2025-10-07 12:23:11 -0600

    Contains the following confirmations
    - AGP and its dependencies were updated.
    - API 36

--- app/src/main/java/in/aabhasjindal/otpview/MainActivity.kt
+++ app/src/main/java/in/aabhasjindal/otpview/MainActivity.kt
@@ -1,14 +1,11 @@
 package `in`.aabhasjindal.otpview
 
 import android.os.Bundle
-import android.view.WindowManager
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import com.github.leodan11.otptextview.OTPListener
 import com.github.leodan11.otptextview.OtpTextView
-import es.dmoral.toasty.Toasty
 
 class MainActivity : AppCompatActivity() {
     private var otpTextView: OtpTextView? = null
@@ -16,9 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val window = window
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-        window.statusBarColor = ContextCompat.getColor(applicationContext, (R.color.black))
         val errorButton = findViewById<Button>(R.id.button)
         val successButton = findViewById<Button>(R.id.button2)
         val valueButton = findViewById<Button>(R.id.button3)
@@ -30,11 +24,11 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onOTPComplete(otp: String) {
-                Toasty.success(this@MainActivity, "The OTP is $otp", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, "The OTP is $otp", Toast.LENGTH_SHORT).show()
             }
         }
         errorButton.setOnClickListener { otpTextView?.showError() }
         successButton.setOnClickListener { otpTextView?.showSuccess() }
-        valueButton.setOnClickListener {  Toasty.info(this@MainActivity, "The OTP is ${otpTextView?.otp}", Toast.LENGTH_SHORT).show() }
+        valueButton.setOnClickListener {  Toast.makeText(this@MainActivity, "The OTP is ${otpTextView?.otp}", Toast.LENGTH_SHORT).show() }
     }
 }
